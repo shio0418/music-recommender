8f3cf9e14bfca8a855a7867eab8948a1161338b5
古宮詩桜 <furumiya.s.aa@m.titech.ac.jp>
2026-04-02 00:05:58 +0900

    feat: 簡単なsongsのGET, POSTメソッドの追加

--- main.go
+++ main.go
@@ -1,1 +1,56 @@
 package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Song struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Artist string `json:"artist"`
+	Tag    string `json:"tag"`
+}
+
+var songs []Song
+var nextID = 1
+
+func main() {
+	e := echo.New()
+
+	e.POST("/songs", postSongHandler)
+	e.GET("/songs", getSongHandler)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func postSongHandler(c echo.Context) error {
+	var song Song
+
+	contentType := c.Request().Header.Get(echo.HeaderContentType)
+	if !strings.HasPrefix(contentType, echo.MIMEApplicationJSON) {
+		return echo.NewHTTPError(http.StatusUnsupportedMediaType, "Content-Type must be application/json")
+	}
+
+	decoder := json.NewDecoder(c.Request().Body)
+	if err := decoder.Decode(&song); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid JSON body")
+	}
+
+	if strings.TrimSpace(song.Title) == "" || strings.TrimSpace(song.Artist) == "" {
+		return echo.NewHTTPError(http.StatusBadRequest, "title and artist are required")
+	}
+
+	song.ID = nextID
+	nextID++
+	songs = append(songs, song)
+
+	return c.JSON(http.StatusCreated, song)
+}
+
+func getSongHandler(c echo.Context) error {
+	return c.JSON(http.StatusOK, songs)
+}
